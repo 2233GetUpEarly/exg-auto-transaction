26ce3b03b53c144e8161b67040fad325af4e2cd5
GetUpEarly <3304968099@qq.com>
2026-05-30 20:49:29 +0800

    网站交互模块部分步骤时间拉长调整

--- website-interaction/initialization-step.js
+++ website-interaction/initialization-step.js
@@ -5,8 +5,8 @@ var T = darkrp.trigger;
 T.step('登录', async function()
 {
     darkrp.button.登录();
-    console.log('✅ 步骤执行：' + '登录' + '，等待 500 ms');
-    await darkrp.util.sleepMillisecond(500);
+    console.log('✅ 步骤执行：' + '登录' + '，等待 2000 ms');
+    await darkrp.util.sleepMillisecond(2000);
 });
 
 T.step('离线登录', async function()
@@ -26,15 +26,15 @@ T.step('弹窗确定', async function()
 T.step('积分商城', async function()
 {
     darkrp.button.积分商城();
-    console.log('✅ 步骤执行：' + '积分商城' + '，等待 800 ms');
-    await darkrp.util.sleepMillisecond(800);
+    console.log('✅ 步骤执行：' + '积分商城' + '，等待 2000 ms');
+    await darkrp.util.sleepMillisecond(2000);
 });
 
 T.step('交易市场', async function()
 {
     darkrp.button.交易市场();
-    console.log('✅ 步骤执行：' + '交易市场' + '，等待 700 ms');
-    await darkrp.util.sleepMillisecond(700);
+    console.log('✅ 步骤执行：' + '交易市场' + '，等待 2000 ms');
+    await darkrp.util.sleepMillisecond(2000);
 });
 
 // ------------------ 买交易币操作部分 -----------------------
@@ -42,8 +42,8 @@ T.step('交易市场', async function()
 T.step('买交易币', async function()
 {
     darkrp.button.买交易币();
-    console.log('✅ 步骤执行：' + '买交易币' + '，等待 500 ms');
-    await darkrp.util.sleepMillisecond(500);
+    console.log('✅ 步骤执行：' + '买交易币' + '，等待 2500 ms');
+    await darkrp.util.sleepMillisecond(2500);
 });
 
 T.step('卖交易币', async function()
@@ -86,22 +86,22 @@ T.step('卖交易币通知成功弹窗关闭', async function()
 T.step('买积分', async function()
 {
     darkrp.button.买积分();
-    console.log('✅ 步骤执行：' + '买积分' + '，等待 500 ms');
-    await darkrp.util.sleepMillisecond(500);
+    console.log('✅ 步骤执行：' + '买积分' + '，等待 1500 ms');
+    await darkrp.util.sleepMillisecond(1500);
 });
 
 T.step('卖积分', async function()
 {
     darkrp.button.卖积分();
-    console.log('✅ 步骤执行：' + '卖积分' + '，等待 1000 ms');
-    await darkrp.util.sleepMillisecond(1000);
+    console.log('✅ 步骤执行：' + '卖积分' + '，等待 1300 ms');
+    await darkrp.util.sleepMillisecond(1300);
 });
 
 T.step('卖积分提交', async function()
 {
     darkrp.button.卖积分提交();
-    console.log('✅ 步骤执行：' + '卖积分提交' + '，等待 1000 ms');
-    await darkrp.util.sleepMillisecond(1000);
+    console.log('✅ 步骤执行：' + '卖积分提交' + '，等待 1300 ms');
+    await darkrp.util.sleepMillisecond(1300);
 });
 
 T.step('卖积分确认上架', async function()
