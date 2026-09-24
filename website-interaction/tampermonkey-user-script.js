78b13431be32adb68e32db596c505b824368755f
GetUpEarly <3304968099@qq.com>
2026-09-24 14:32:48 +0800

    增加油猴脚本 UserScript 部分

---
+++ website-interaction/tampermonkey-user-script.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         DarkRP 自动化脚本
+// @namespace    http://tampermonkey.net/
+// @version      2026-09-24-0.2.0
+// @description  DarkRP 自动登录、导航、买卖积分
+// @author       GetUpEarly
+// @match        http://list.darkrp.cn:6514/*
+// @match        https://list.darkrp.cn:6514/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
